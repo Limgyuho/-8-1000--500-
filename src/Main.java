af4f50f63328e21bff009cee95934e019750472c
Limgyuho <radum0719@hanmail.net>
2022-10-29 20:57:12 +0900

    구구단8단 1000부터 -500까지 출력하기

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int dan = 8;
+		
+		int i =1000;
+		
+		while(i>=-500) {
+			
+			System.out.printf("%d*%d=%d\n",dan,i,dan*i);
+			
+			i--;
+		}
+	}
+
+}
